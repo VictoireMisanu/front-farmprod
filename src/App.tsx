13aa94d43cc046e977eb8125dd5624d430841c1d
victoire <vicmisanu63@gmail.com>
2024-12-03 11:25:50 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+
+
+function App() {
+
+  return (
+      <div className=" h-screen w-full bg-slate-300 flex justify-center items-center">
+        <img src="/logo/logoApp.png" alt="farmProd"  />
+      </div>
+  )
+}
+
+export default App
