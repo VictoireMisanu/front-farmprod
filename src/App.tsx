853b00967fd209c81738f29344c7e896f527aa67
Vic <vicmisanu63@gmail.com>
2024-12-03 17:34:36 +0100

    develop cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 //import { useState } from 'react'
 
-import Header from "./components/header/header"
+
 
 
 function App() {
 
   return (
       <>
-      <Header className = "bg-[#C7DDB5] w-full h-10 flex items-center">
-        
-      </Header>
+      
       </>
   )
 }
