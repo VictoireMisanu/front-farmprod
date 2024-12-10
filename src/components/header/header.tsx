1a9e53cbeee9d4753a209689ace19a69817069f0
Vic <vicmisanu63@gmail.com>
2024-12-10 03:30:25 +0100

    hover effects added on the header items

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -12,14 +12,14 @@ const Header = ({className}:headerProps) => {
         <div className={className}>
             <Logo/>
                 <div id="navElement" className="w-42 h-full flex flex-row items-center justify-center px-10 gap-12">
-                    <a href="#" className="text-md text-[#5B8C51] font-normal">Acceuil</a>
-                    <a href="#" className="text-md text-black  font-normal hover:text-[#5B8C51]">A propos</a>
-                    <a href="#" className="text-md text-black  font-normal hover:text-[#5B8C51]">Produits</a>
-                    <a href="#" className="text-md text-black  font-normal hover:text-[#5B8C51]">Fermiers</a>
+                    <a href="#" className="text-md text-[#5B8C51] p-2 rounded-lg hover:bg-[#FEF3B8] font-normal">Acceuil</a>
+                    <a href="#" className="text-md text-black  font-normal bg-none p-2 rounded-lg hover:bg-[#FEF3B8] hover:text-[#5B8C51]">A propos</a>
+                    <a href="#" className="text-md text-black  font-normal bg-none p-2 rounded-lg hover:bg-[#FEF3B8] hover:text-[#5B8C51]">Produits</a>
+                    <a href="#" className="text-md text-black  font-normal bg-none p-2 rounded-lg hover:bg-[#FEF3B8] hover:text-[#5B8C51]">Fermiers</a>
                      
                 </div>
                 <div id="btnSection" className="h-full w-[22rem] flex flex-row items-center justify-center px-10 gap-5">
-                    <div className="w-1/2 h-1/2 bg-[#EDDD5E] rounded-2xl p-3 flex justify-center items-center hover:bg-[#FFC107]">
+                    <div className="w-1/2 h-1/2 bg-[#EDDD5E] rounded-2xl p-3 flex justify-center items-center hover:bg-[#FFC107] hover:shadow-lg shadow-black">
                         <a href="#" className="md:text-sm lg:text-md text-black font-normal">Commander</a>
                     </div>
 
