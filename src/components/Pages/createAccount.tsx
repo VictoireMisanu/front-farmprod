7dca13c63fd137f5fa6e67f32951bb07a486c2a2
Vic <vicmisanu63@gmail.com>
2024-12-20 18:32:42 +0100

    feat-socialAuth options added on createAccount page

--- src/components/Pages/createAccount.tsx
+++ src/components/Pages/createAccount.tsx
@@ -21,25 +21,37 @@ const CreateAccount = () => {
             </div>
             <form className='bg-white flex flex-col justify-center items-center gap-10'>
                 <div id="title">
-                    <p className="font-extrabold text-black leading-10 text-2xl mb-2">S'enregistrer</p>
+                    <p className="font-extrabold text-black leading-10 text-2xl mb-2">S'inscrire</p>
                 </div>
                 <div id='middleSection' className='w-3/4 h-auto flex flex-col items-center gap-5'>
                     <div id='profilePic' className="h-32 w-32 rounded-full border-2 border-[#658221] flex flex-col justify-items-center">
-                        <input className="w-full h-full hover:cursor-pointer opacity-0" type="file" title='Photo de profil' id="profilePic" {...register('profilePic')}/>
+                        <input className="w-full h-full hover:cursor-pointer opacity-0" type="file" title='Photo de profil' id="profilePic" {...register('profilePic')} name='profilePic'/>
                     </div>
                     <div id='userName' className='w-1/2 h-7 flex justify-items-center'>
-                        <input className='bg-transparent border-b-2 border-b-[#658221] w-full h-full p-5 outline-none' placeholder='Prenom et nom' type="text" {...register('username')}/>
+                        <input className='bg-transparent border-b-2 border-b-[#658221] w-full h-full p-5 outline-none' placeholder='Prenom et nom' type="text" {...register('username')} name='userName'/>
                     </div>
                     <div id='emailAddress'className='w-1/2 h-7 flex justify-items-center rounded-md'>
-                        <input className='bg-transparent border-b-2 border-[#658221] w-full h-full p-5 outline-none' placeholder='Adresse email' type="email" {...register('email')}/>
+                        <input className='bg-transparent border-b-2 border-[#658221] w-full h-full p-5 outline-none' placeholder='Adresse email' type="email" {...register('email')} name='emailAddress'/>
                     </div>
                     <div id='password' className='w-1/2 h-7 flex justify-items-center rounded-md'>
-                        <input className='bg-transparent border-b-2 border-[#658221] w-full h-full p-5 outline-none' placeholder='Mot de passe'  type="password" {...register('password')}/>
+                        <input className='bg-transparent border-b-2 border-[#658221] w-full h-full p-5 outline-none' placeholder='Mot de passe'  type="password" {...register('password')} name='password'/>
                     </div>
                 </div>
                 <input className='bg-[#658221] w-1/2 h-12 rounded-md text-white font-bold hover:cursor-pointer hover:text-[#658221] hover:border-[#658221] border-2 hover:bg-transparent hover:shadow-lg shadow-black' type="submit" value="S'enregistrer" />
-
+                <div id='socialMedia' className='w-full h-32 flex flex-col gap-5 justify-center items-center'>
+                    <div className='w-1/2 h-auto flex flex-row gap-3 justify-center items-center'>
+                        <hr className=' border-slate-700 w-1/2'/>
+                        <span>Ou</span>
+                        <hr className='border-slate-700 w-1/2'/>
+                    </div>
+                    <div id='socialMedia' className='w-1/2 h-auto flex flex-row items-center justify-center gap-8'>
+                        <Link to="#"><img src="/svg/google.svg" alt="Google" title='Créer un compte avec google' /></Link>
+                        <Link to="#"><img src="/svg/facebook.svg" alt="Google" title='Créer un compte avec google' /></Link>
+                        <Link to="#"><img src="/svg/apple.svg" alt="Google" title='Créer un compte avec google' /></Link>
+                    </div>
+                </div>
             </form>
+
         </div>
         
     )
