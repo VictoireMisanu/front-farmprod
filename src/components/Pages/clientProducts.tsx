dd754b03c9c150b37154f8f8a7de456cb59f9de3
Vic <vicmisanu63@gmail.com>
2024-12-24 09:46:07 +0100

    feat-clientProduct header page added

--- src/components/Pages/clientProducts.tsx
+++ src/components/Pages/clientProducts.tsx
@@ -6,17 +6,19 @@ import SimpleLink from "../link&btn/simpleLink"
 const ClientProducts = () => {
     return (
         <>
-            <Header className ="bg-[#C7DDB5] w-full h-20 flex justify-between items-center pl-16 shadow-md shadow-black/20 fixed">
-                <nav>
+            <Header className ="w-full h-20 bg-[#C7DDB5] shadow-md shadow-black/20 fixed px-10">
+                <nav className="w-full h-full flex flex-row justify-between items-center ">
                     <div id="part1" className="flex flex-row items-center gap-5">
                         <button><img src="/svg/burger.svg" alt="" /></button>
                         <SimpleLink to="" className="bg-[#EDDD5E] flex items-center justify-center h-12 w-12 p-2 rounded-full"><img src="/svg/arrow.svg" alt="" /></SimpleLink>
                     </div>
-                    <div id="part2" className="">
-
+                    <div id="part2" className="bg-white w-auto h-1/2 shadow-lg shadow-black/20 rounded-xl flex flex-row justify-between items-center px-2">
+                        <label htmlFor="search"><img src="/svg/search.svg" alt="" className="w-7 h-7"/></label>
+                        <input type="search" name="searchInput" id="search" className="w-2/3 outline-none" placeholder="Rechercher"/>
                     </div>
-                    <div id="part3" className="">
-
+                    <div id="part3" className="flex flex-row items-center gap-5">
+                        <SimpleLink to="" className="flex items-center justify-center pr-5 border-black border-r-[1px]"><img src="/svg/basket.svg" alt="" className="w-10 h-10"/></SimpleLink>
+                        <SimpleLink to="" className="flex items-center justify-center "><img src="/svg/user.svg" alt="" className="w-10 h-10"/></SimpleLink>
                     </div>
                 </nav>
             </Header>
